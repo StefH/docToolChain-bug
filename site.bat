d4ad8fd1d32711d07de041ed9d3183c8df3ab9ed
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-10-11 19:09:06 +0200

    Initial

---
+++ site.bat
@@ -0,0 +1,4 @@
+cls
+rd /s /q ".gradle"
+rd /s /q "build"
+docker run --rm --entrypoint /bin/bash -it -v %cd%:/project docker.io/doctoolchain/doctoolchain:v3.3.1 -c "doctoolchain . generateSite -PmainConfigFile=config/docToolchainConfig.groovy --warning-mode=none --no-daemon && exit"
